b14cc525fc600146532a358e8ea85eb528d3d49c
Devendra Bhattarai <91841677+Deven2057@users.noreply.github.com>
2023-05-16 16:46:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 first.insertAdjacentHTML('beforebegin', '<div class="text">  beforebegin </div> ' )
 first.insertAdjacentHTML('beforeend', '<div class="text">  before end </div> ' )
-first.insertAdjacentHTML('afterbegin', '<div class="text">  after begin </div> ' )
+first.insertAdjacentHTML('afterbegin', '<div class="text">  after begin of tag </div> ' )
 first.insertAdjacentHTML('afterend', '<div class="text">  after end </div> ' )
